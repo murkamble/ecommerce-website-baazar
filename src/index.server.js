9945d2f6b9a7e87dfe969bcb151e16021758a5c2
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-04-26 16:44:03 +0530

    MongoDB Connection.

--- src/index.server.js
+++ src/index.server.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const env = require('dotenv')
+const mongoose = require('mongoose')
 
 // environment variable or you can say constants
-const app = express();
-env.config();
+const app = express()
+env.config()
+app.use(express.json())
+
+// mongoose Databases connection string
+mongoose.connect(
+  // 'mongodb://localhost:27017/baazar', 
+  'mongodb+srv://root:root@cluster0.a0xhf.mongodb.net/inBaazaar?retryWrites=true&w=majority',
+  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+  }
+).then(() => {
+  console.log('Databases Connected.')
+})
 
 // 
 app.get("/", (req, res) => {
@@ -11,5 +26,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`)
 });
