d3a5880a9850dd056b9c8b5ba2c3cf86d071839a
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-04-27 18:47:43 +0530

    Signin And Signup Form Design In Admin

--- client_admin/src/containers/Signin/index.js
+++ client_admin/src/containers/Signin/index.js
@@ -1,12 +1,35 @@
 import React from 'react'
-import { Container } from "react-bootstrap";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
 import Layout from "../../components/Layout";
+import Input from "../../components/UI/Input";
 
 const Signin = (props) => {
     return (
         <Layout>
             <Container>
-                signin
+                <Row style={{ marginTop: '50px' }}>
+                    <Col md={{ span: 6, offset: 3 }}>
+                        <Form>
+                            <Input
+                                label="Email Address"
+                                placeholder="Email Address"
+                                value=""
+                                type="email"
+                                onChange={() => {}}
+                            />
+                            <Input
+                                label="Password"
+                                placeholder="Password"
+                                value=""
+                                type="password"
+                                onChange={() => {}}
+                            />
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
             </Container>
         </Layout>
     )
