c35db619f1766fbce49a524026d249bcfbb0f5f7
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-04-27 21:28:28 +0530

    API Request Validation

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
 const { signup, signin } = require('../../controller/admin/auth');
+const { validateSignupRequest, validateSigninRequest, isRequestValidated } = require('../../validators/auth')
 
-router.post('/admin/signup', signup)
-router.post('/admin/signin', signin)
+router.post('/admin/signup', validateSignupRequest, isRequestValidated, signup)
+router.post('/admin/signin', validateSigninRequest, isRequestValidated, signin)
 
 module.exports = router;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router();
-const { signup, signin, requireSignin } = require('../controller/auth');
+const { signup, signin } = require('../controller/auth');
+const { validateSignupRequest, validateSigninRequest, isRequestValidated } = require('../validators/auth')
 
-router.post('/signup', signup)
-router.post('/signin', signin)
+router.post('/signup', validateSignupRequest, isRequestValidated, signup)
+router.post('/signin', validateSigninRequest, isRequestValidated, signin)
 
+// 
 // router.post('/profile', requireSignin, (req, res) => {
 //     res.status(200).json({ message: 'profile' })
 // })
