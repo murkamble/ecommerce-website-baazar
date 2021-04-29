16b67d405ab77739f1f547f20bc6639b8fc60996
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-04-29 11:41:27 +0530

    Admin Login & Private Route

--- client_admin/src/App.js
+++ client_admin/src/App.js
@@ -4,13 +4,14 @@ import Home from "./containers/Home";
 import Signin from "./containers/Signin";
 import Signup from "./containers/Signup";
 import './App.css';
+import PrivateRoute from "./components/HOC/PrivateRoute";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-        <Route path='/' exact component={Home} />
+        <PrivateRoute path='/' exact component={Home} />
         <Route path='/Signin' component={Signin} />
         <Route path='/Signup' component={Signup} />
         </Switch>
--- client_admin/src/containers/Signin/index.js
+++ client_admin/src/containers/Signin/index.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Form, Row, Col, Button } from "react-bootstrap";
 import Layout from "../../components/Layout";
 import Input from "../../components/UI/Input";
-import { login } from "../../actions";
-import { useDispatch } from "react-redux";
+import { login, isUserLoggedIn } from "../../actions";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from 'react-router-dom';
 
 const Signin = (props) => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const auth = useSelector(state => state.auth);
+    
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(!auth.authenticate){
+            dispatch(isUserLoggedIn());
+        }
+    }, [])
 
     const userLogin = (e) => {
         e.preventDefault()
         const user = { email, password }
         dispatch(login(user))
+    }
+
+    if(auth.authenticate){
+        return <Redirect to={'/'} />
     }
 
     return (
