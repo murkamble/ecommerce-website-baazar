4a34b17aa380b6ab584f2dd11eb42804af2de040
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-06 16:54:27 +0530

    Flipkart Header Clone

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './style.css';
+import flipkartLogo from '../../images/logo/flipkart.png';
+import loginImage from '../../images/login-image.png';
+import goldenStar from '../../images/logo/golden-star.png';
+import { IoIosArrowDown, IoIosCart, IoIosSearch } from 'react-icons/io';
+import {
+  Modal,
+  MaterialInput,
+  MaterialButton,
+  DropdownMenu
+} from '../MaterialUI';
+import { login, signout, getCartItems, signup as _signup } from "../../actions";
+import { useDispatch, useSelector } from "react-redux";
+
+
 
 /**
 * @author
@@ -7,12 +21,155 @@ import './style.css';
 **/
 
 const Header = (props) => {
-  return (
-    <div className='header'>
 
+  const [loginModal, setLoginModal] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+
+  const userLogin = () => {
+    dispatch(login({ email, password }));
+
+  };
+
+
+  return (
+    <div className="header">
+      <Modal
+        visible={loginModal}
+        onClose={() => setLoginModal(false)}
+      >
+        <div className="authContainer">
+          <div className="row">
+            <div className="leftspace">
+              <h2>Login</h2>
+              <p>Get access to your Orders, Wishlist and Recommendations</p>
+            </div>
+            <div className="rightspace">
+
+
+              <MaterialInput
+                type="text"
+                label="Enter Email/Enter Mobile Number"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <MaterialInput
+                type="password"
+                label="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              // rightElement={<a href="#">Forgot?</a>}
+              />
+              <MaterialButton
+                title="Login"
+                bgColor="#fb641b"
+                textColor="#ffffff"
+                style={{
+                  margin: '20px 0'
+                }}
+                onClick={userLogin}
+              />
+              <p style={{ textAlign: "center" }}>OR</p>
+              <MaterialButton
+                title="Request OTP"
+                bgColor="#ffffff"
+                textColor="#2874f0"
+                style={{
+                  margin: "20px 0",
+                }}
+              />
+
+
+
+            </div>
+          </div>
+        </div>
+      </Modal>
+      <div className="subHeader">
+        <div className="logo">
+          <a href="/" style={{
+            color: '#fff',
+            textDecoration: 'none',
+            fontSize: '24px',
+            padding: '10px',
+            fontWeight: '500'
+          }}>
+            <label>Baazar</label>
+          </a>
+          {/* <a style={{ marginTop: '-10px' }}>
+            <span className="exploreText">Explore</span>
+            <span className="plusText">Plus</span>
+            <img src={goldenStar} className="goldenStar" alt="" />
+          </a> */}
+        </div>
+        <div style={{
+          padding: '0 10px'
+        }}>
+          <div className="searchInputContainer">
+            <input
+              className="searchInput"
+              placeholder={'search for products, brands and more'}
+            />
+            <div className="searchIconContainer">
+              <IoIosSearch style={{
+                color: '#2874f0'
+              }} />
+            </div>
+
+          </div>
+        </div>
+        <div className="rightMenu">
+          <DropdownMenu
+            menu={
+              <a className="loginButton" onClick={() => setLoginModal(true)}>
+                Login
+              </a>
+            }
+            menus={[
+              { label: 'My Profile', href: '', icon: null },
+              { label: 'Flipkart Plus Zone', href: '', icon: null },
+              { label: 'Orders', href: '', icon: null },
+              { label: 'Wishlist', href: '', icon: null },
+              { label: 'Rewards', href: '', icon: null },
+              { label: 'Gift Cards', href: '', icon: null },
+            ]}
+            firstMenu={
+              <div className="firstmenu">
+                <span>New Customer?</span>
+                <a style={{ color: '#2874f0' }}>Sign Up</a>
+              </div>
+            }
+          />
+          <DropdownMenu
+            menu={
+              <a className="more">
+                <span>More</span>
+                <IoIosArrowDown />
+              </a>
+            }
+            menus={[
+              { label: 'Notification Preference', href: '', icon: null },
+              { label: 'Sell on flipkart', href: '', icon: null },
+              { label: '24x7 Customer Care', href: '', icon: null },
+              { label: 'Advertise', href: '', icon: null },
+              { label: 'Download App', href: '', icon: null }
+            ]}
+          />
+          <div>
+            <a className="cart">
+              <IoIosCart />
+              <span style={{ margin: '0 10px' }}>Cart</span>
+            </a>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
-}
 
+}
 
 export default Header
